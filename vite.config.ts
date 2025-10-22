d64229cf449984e82a87d312ca61a7617d44d789
Nicolay Moreno <nicolaymorenoherrera@gmail.com>
2025-10-21 21:53:51 -0500

    Consulta a la api ok

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// Configuración moderna (Tailwind v4)
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
